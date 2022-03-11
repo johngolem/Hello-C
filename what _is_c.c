5a4297a1eb0d7c5948dee4e1e4a59029e34cf38b
John Paul <johnpaul@trees.local>
2022-03-11 12:56:55 +0300

    some basic info on c

---
+++ what _is_c.c
@@ -0,0 +1,34 @@
+C is an imperative (procedural) language. 
+All the code has to be inside a function. 
+C files are source code of your program. 
+You have to compile C files with a compiler (for instance gcc) to create an executable file
+Comments Begins with /* and ends with */
+Can be inserted anywhere a white-space character is allowed. 
+Comments dont nest Use comments to document your code
+
+char -1 byte-128 to 127(VALUE RANGE)
+unsigned char1 byte0 to 255
+ short2 bytes-32,768 to 32,767 
+unsigned short2 bytes0 to 65,535 
+int4 bytes-2,147,483,648 to 2,147,483,647 
+unsigned int4 bytes0 to 4,294,967,295 
+long8 bytes−9,223,372,036,854,775,808 to 9,223,372,036,854,775,807 
+unsigned long8 bytes
+
+
+Declaration
+ Variables always have a type. 
+ Syntax: type var_name; for
+example : int 45;
+char i;
+
+
+A program is a collection of functions
+Functions: 
+●Sequence of program instructions that perform a specific task, packaged as a unit 
+●May take arguments as input information 
+●Compute something 
+●May return a result 
+●Functions can call functions
+
+Parameters and return values must have a type
