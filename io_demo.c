041a1bcbbe064447204573154292ddc1ed6b7ed3
John Paul <johnpaul@trees.local>
2022-03-11 12:19:26 +0300

    pront for name and age

--- io_demo.c
+++ io_demo.c
@@ -6,6 +6,8 @@ int main (void)
 	
 	char f,m,l;
 	int age;
+	
+	printf ("Enter your initials, followed by your age: ");
 	scanf ("%c %c %c %d", &f, &m, &l, &age);
 	printf("My initials are:%c%c%c and my age is %d. \n" ,f,m,l,age);
 	return (0);
