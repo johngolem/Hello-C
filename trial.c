f4cfd1bd8e235a1875e5e320dc15fbd0e6017b8d
John Paul <johnpaul@trees.local>
2022-03-19 04:21:36 +0300

    scanf practise

---
+++ trial.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int m, n;
+    float ch;
+    float bh;
+    
+    scanf("input the numbers: " %d %d", &m, &n);
+    scanf ("%f %f", &ch, &bh);
+
+    return 0;
+}
